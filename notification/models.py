4126585557e8a3c1dc6abbf8bad5217a6b48077c
LAPTOP-INM6L1QK\SW중심대학사업단 <>
2022-07-18 21:45:15 +0900

    no message

--- notification/models.py
+++ notification/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+import requests
+from bs4 import BeautifulSoup
 
+html = requests.get("https://cyber.wku.ac.kr/Cyber/ComBoard_V005/Content/list.jsp?gid=1115983888724&bid=1115985252888")
+html.raise_for_status()
+html.encoding='utf-8'
+bsObject = BeautifulSoup(html.content, "html.parser")
 
-class Question(models.Model):
+print(bsObject.table.tbody)
+
+'''class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -9,4 +17,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)'''
+
+    
