39b4402a4f69ca5baba8512af8345f5022b4dccc
LimDongHwi <dlaehdgnl02@gmail.com>
2022-07-18 21:43:03 +0900

    v 1.3

--- notification/views.py
+++ notification/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the notification index")
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'notification/index.html', context)
